b0f618ad57b49ad3797d91e96dcbdc4898e8f014
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2022-11-18 16:26:23 +0900

    sheesh

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Header from "@/components/header"
-import Hero from "@/components/hero"
-import Footer from "@/components/footer"
-import Layout from './components/layout'
 
 export default function Home() {
   return (
