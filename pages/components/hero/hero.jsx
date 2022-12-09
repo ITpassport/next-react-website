75bcccba1907616d8f02821a448fdd7065c73c7d
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2022-12-09 15:08:06 +0900

    結果にcomit

--- pages/components/hero/hero.jsx
+++ pages/components/hero/hero.jsx
@@ -1,11 +1,11 @@
-import style from "style/hero.module.css"
+import styles from "./hero.module.css"
 
 export default function Hero({title,subtitle,imageOn=false}) {
 return (
     <div>
-        <div className={style.text}>
+        <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
-        <p className={style.subtitle}>{subtitle}</p>
+        <p className={styles.subtitle}>{subtitle}</p>
     </div>
     {imageOn && <figure>[画像]</figure>}
     </div>
